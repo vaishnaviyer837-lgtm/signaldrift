fdfa35c195eec597f701de4334c881103c586e1a
vaishnaviyer837-lgtm <vaishnav.iyer.837@gmail.com>
2026-05-19 23:37:04 -0500

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import { AppProvider } from './context/AppContext';
 import './styles/global.css';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AppProvider>
         <App />
+        <Analytics />
       </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
